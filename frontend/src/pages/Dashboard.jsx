b5799acd2322dc8475caaebf55f629443a86ba45
Jerry Lin <z5363361@ad.unsw.edu.au>
2024-04-17 15:28:12 +1000

    got the scrolling stuff working

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -32,7 +32,7 @@ function Dashboard ({ token, setTokenFunc }) {
   return (
       <div id='background' style= {{ height: '100%', width: '100%', padding: '0', margin: '0', backgroundColour: 'red' }}>
         <NewPresModal open = {viewModal} closeModal={() => setViewModal(false)} token={token}/>
-        <Grid container spacing={0}>
+        <Grid container spacing={0} style= {{ height: '100%' }}>
           <Grid item xs={12} style= {{ height: '10vh', backgroundColor: '#dbeafe' }}>
               <div id="logout-button-div"style= {{ float: 'right' }}>
                 <Button variant="contained" onClick={() => setViewModal(true)}>+ New Presentation</Button>
@@ -40,7 +40,7 @@ function Dashboard ({ token, setTokenFunc }) {
               </div>
               <h1 id="Presto-Logo" style= {{ float: 'left', fontFamily: 'Arial', color: '#1e3a8a' }}>Presto</h1>
           </Grid>
-          <Grid item xs={12} style= {{ height: '85vh', backgroundColor: '#bfdbfe', display: 'flex', flexWrap: 'wrap', paddingTop: '1%' }}>
+          <Grid item xs={12} style= {{ height: '85vh', backgroundColor: '#bfdbfe', display: 'flex', flexWrap: 'wrap', paddingTop: '1%', overflowY: 'auto' }}>
             {pressies && Object.entries(pressies).map(pressie => (
               <PresentationCards
                   key={pressie.id}
@@ -51,7 +51,7 @@ function Dashboard ({ token, setTokenFunc }) {
               />
             ))}
           </Grid>
-          <Grid item xs={12} style= {{ height: '5vh', backgroundColor: '#bfdbfe' }}>
+          <Grid item xs={12} style= {{ height: '3vh', backgroundColor: '#bfdbfe', fontFamily: 'Arial', color: '#f97316' }}>
             Created by Jerry Lin and Iqtidar Rahman
           </Grid>
         </Grid>
